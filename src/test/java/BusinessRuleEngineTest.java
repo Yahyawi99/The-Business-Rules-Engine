d4edac51d62bd4c62649ef9fbb9a3cbb89b5a538
Yahyawi99 <yassinyahyawi26@gmail.com>
2026-06-04 19:03:49 +0100

    TDD

---
+++ src/test/java/BusinessRuleEngineTest.java
@@ -0,0 +1,18 @@
+public class BusinessRuleEngineTest {
+  @Test
+  void shouldHaveNoRulesInitially() {
+    final BusinessRuleEngine businessRuleEngine = new BusinessRuleEngine();
+
+    assertEquals(0, businessRuleEngine.count());
+  }
+
+  @Test
+  void shouldAddTwoActions() {
+    final BusinessRuleEngine businessRuleEngine = new BusinessRuleEngine();
+
+    businessRuleEngine.addAction(()=>{});
+    businessRuleEngine.addAction(()=>{});
+
+    assertEquals(2, businessRuleEngine.count());
+  }
+}
